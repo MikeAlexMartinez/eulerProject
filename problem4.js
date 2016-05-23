b84d2fc238fec30ed01556f98eb0968f1624904f
Michael Martinez <mike.alex.martinez@gmail.com>
2016-05-23 18:12:54 +0100

    amended problem4 algorithm
    Also added excel sheet which I used to look at pattern for palindromes

--- problem4.js
+++ problem4.js
@@ -1,20 +1,73 @@
 /* find the largest palindrome made from 2 3 digit numbers. */
 
-/* steps 1: largest number from 2 3 digit numbers is 999*999 ergo, palindrome must be smaller. */
-/* step 2: loop through numbers from number above downwards evaluating if number is palindrome */
-/* step 3: if palindrome, can the number be divisble by two whole factors? start from 999 decrement to 100 */
-/* Note: 	smallest divisor must be at least as big at the sqrt of the palindrome, else the answer may be 
-			bigger than a three digit number */
-var testNumber = 999;
-var firstTestNumber = 999^2;
+/* step 1: largest 6 digit palindrome is 999999. */
+/* step 2: working through each palindrome, every iteration is 1100 less than the last, except every
+			10th iteration is 110 and every 100th iteration is 11 less.
+			Once palindorme is less than 10000, stop */
+/* step 3: Can the palindrome be divisble by two whole factors? start from 999 decrement, until 
+			the second factor must be bigger than 999 in order to be correct */
+
+			
+var start = 999999;
+var iterationDeduction1 = 1100;
+var iterationDeduction10 = 110;
+var iterationDeduction100 = 11;
 var divisor = 999;
-var lowerLimit = 100;
-var palindromeFloor = 10000;
+var palindromeFloor = 100000;
 var answer = 0;
 var factors = [];
+var answerFound = false;
+var iteration = 1;
 
-var palindromeTest = function (number) {
+
+while( start > palindromeFloor ) {
 	
+	console.log("start: " + start);
+	
+	
+	var factorCheck = true;
+	while( factorCheck && divisor > 100 ){
+		
+		var divisorLength =  toString(Math.round(start / divisor)).length;
+		console.log("divisor: " + divisor) ;
+		console.log("div length: " + divisorLength) ;
+		
+		if(divisorLength > 3) {
+			factorCheck = false;
+		} else {
+			if (start % divisor === 0) {
+				factors.push(divisor);
+				factors.push(start / divisor);
+				answer = start;
+				answerFound = true;
+				factorCheck = false;
+			}
+		}
+		
+		divisor--;
+	}	
+		
+	/* this works fine */	
+	if(iteration % 100 === 0){
+        start -= iterationDeduction100;
+    } else if (iteration % 10 === 0){
+	    start -= iterationDeduction10;
+    } else {
+	    start -= iterationDeduction1;
+    }
+
+    iteration++;
+}
+
+if (answerFound){
+	console.log("factors are: " + factors);
+	console.log("answer is: " + answer);
+} else {
+	console.log("Answer not found");
+}
+/* function no longer needed, changed algorithm only uses palindromes */
+/* var palindromeTest = function (number) {
+    
 	var test = false ;
 
 	var testString = number.toString();
@@ -26,35 +79,4 @@ var palindromeTest = function (number) {
 	}
 
 	return test;
-};
-
-
-for ( i = firstTestNumber ; i > 10000 ; i-- ) {
-	
-	var palCheck = palindromeTest(i);
-
-	if(palCheck) {
-
-		for ( j = divisor ; j > lowerLimit ; j-- ) {
-			
-
-			if ( i % j === 0 ) {
-				
-				var
-
-				if (i / j) {
-				answer = i ;
-				factors.push(j);
-				factors.push(i / j);
-
-				j = lowerLimit;
-				i = 10000;
-				}
-			}
-		}
-	}
-}
-
-console.log(factors);
-console.log(answer);
-
+}; */ 
